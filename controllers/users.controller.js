b18589621367f3c4d8adb230a8b391d554fca7f7
farhanacsebd <farhana.cse.bd@gmail.com>
2023-09-04 21:42:38 +0600

    update api done

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -19,4 +19,19 @@ const createUser = (req,res) =>{
     res.status(201).json(users);
 }
 
-module.exports = {getAllUsers, createUser};
+// update user 
+const updateUser = (req,res) =>{
+    
+    const userid = req.params.id;
+    const {username,email} = req.body;
+    users.filter((user) => user.id == userid).map(
+        (selecteduser) =>{
+            selecteduser.username = username;
+            selecteduser.email = email;
+        }
+    )
+
+    res.status(200).json(users);
+}
+
+module.exports = {getAllUsers, createUser , updateUser};
--- routes/users.route.js
+++ routes/users.route.js
@@ -1,10 +1,11 @@
-const {getAllUsers , createUser} = require("../controllers/users.controller");
+const {getAllUsers , createUser, updateUser} = require("../controllers/users.controller");
 
 
 const router = require("express").Router();
 
 router.get("/",getAllUsers)
 router.post("/",createUser)
+router.put("/:id",updateUser)
 
 
 module.exports = router;
