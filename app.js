1fd9bffde006260a89ab4b0fa0ed8efbf0654aa9
farhanacsebd <farhana.cse.bd@gmail.com>
2023-09-04 13:24:22 +0600

    create home route, router err or server err

--- app.js
+++ app.js
@@ -8,5 +8,19 @@ app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
+//home route
+app.get("/",(req,res) => {
+    res.sendFile(__dirname + "/views/index.html")
+});
+
+//route not found error
+app.use((req,res,next) => {
+    res.status(404).json({message: "router not found"});
+});
+//server error
+app.use((err,req,res,next) => {
+    res.status(500).json({message: "something broke"});
+});
+
 
 module.exports = app;
