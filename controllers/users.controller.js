934cf07a5a83de99cc2dced2b36bc925ecd1b078
farhanacsebd <farhana.cse.bd@gmail.com>
2023-09-04 21:22:15 +0600

    post user

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,7 +1,22 @@
-const users = require("../models/users.model")
+let users = require("../models/users.model");
+const { v4: uuidv4 } = require("uuid");
 
+
+// get user
 const getAllUsers = (req,res) =>{
     res.status(200).json({users});
 }
 
-module.exports = getAllUsers;
+
+// create user 
+const createUser = (req,res) =>{
+    const newUser = {
+        id: uuidv4(),
+        username: req.body.username,
+        email: req.body.email,
+    }
+    users.push(newUser)
+    res.status(201).json(users);
+}
+
+module.exports = {getAllUsers, createUser};
--- routes/users.route.js
+++ routes/users.route.js
@@ -1,8 +1,10 @@
-const getAllUsers = require("../controllers/users.controller");
+const {getAllUsers , createUser} = require("../controllers/users.controller");
+
 
 const router = require("express").Router();
 
 router.get("/",getAllUsers)
+router.post("/",createUser)
 
 
 module.exports = router;
